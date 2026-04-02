a1084b6465aae08166a218f849dcf3ecd38368cc
unknown <lariosangel062@gmail.com>
2026-04-02 10:22:41 -0600

    feat: agregar cambios de correccion de enums

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -3,7 +3,6 @@ using ERP.Core.Manager.Api.Infrastructure.Services;
 using ERP.Core.Manager.Api.Infrastructure.Persistence;
 using ERP.Core.Manager.Api.Infrastructure.Persistence.Context;
 using ERP.Core.Manager.Api.Infrastructure.Persistence.Repositories;
-using ERP.Core.Manager.Api.Infrastructure.Persistence.Configurations.Database;
 
 using ERP.Core.Manager.Api.Domain.Interfaces;
 using ERP.Core.Manager.Api.Application.Commons.Interfaces;
