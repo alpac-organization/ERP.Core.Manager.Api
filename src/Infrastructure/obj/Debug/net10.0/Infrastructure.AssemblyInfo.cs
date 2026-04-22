ce2bc0dadfd55a713454c868c61fb8956170d022
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-22 09:55:18 -0600

    feat: agregar correccion de la base de datos de sql

--- src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98abca1f20b16e055de5671944464e907de23fb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72f3d365fc06f8e6ac7004fe1e20442c1b5c8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
