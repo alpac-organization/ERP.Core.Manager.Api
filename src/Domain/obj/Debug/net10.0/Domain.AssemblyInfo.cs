25c2c26aa9b5c8531300a647efb4e3502b5f15fe
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-24 10:49:14 -0600

    feat: agregar chromium para generador de documentos

--- src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777c002f3e43b4a3b1f19f69ecb1112ad790d2aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03e2583dd2d0981e39e060909d28d3debef56ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
