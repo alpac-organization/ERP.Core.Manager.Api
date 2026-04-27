2b578f8fc79b5319cdf7b96808536e9424a72044
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-27 15:59:20 -0600

    feat: guardar relacion de tablas de ingresos

--- src/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b240b5020d5212f42029db90356a15d9716e6f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb5006351be1e7849fae57858c7887a6ed7346df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ERP.Core.Manager.Api/obj/Debug/net10.0/ERP.Core.Manager.Api.AssemblyInfo.cs
+++ src/ERP.Core.Manager.Api/obj/Debug/net10.0/ERP.Core.Manager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b240b5020d5212f42029db90356a15d9716e6f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb5006351be1e7849fae57858c7887a6ed7346df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
