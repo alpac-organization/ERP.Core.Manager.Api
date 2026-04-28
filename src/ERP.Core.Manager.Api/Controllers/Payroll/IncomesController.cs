b078c1994a3c678fd0427e456d0aa1c196450c55
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-27 20:26:24 -0600

    chore: guardar cambios de incomes

--- src/ERP.Core.Manager.Api/Controllers/Payroll/IncomesController.cs
+++ src/ERP.Core.Manager.Api/Controllers/Payroll/IncomesController.cs
@@ -1,11 +1,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using ERP.Core.Database.Domain.Enums;
 using ERP.Core.Domain.Entities.Errors;
 using ERP.Core.Manager.Api.Controllers.ApiBase;
 using ERP.Core.Manager.Api.Infrastructure.Attributes;
-using ERP.Core.Manager.Api.Application.Features.Deductions.v1.Dtos;
-using ERP.Core.Manager.Api.Application.Features.Deductions.v1.Queries;
 using ERP.Core.Manager.Api.Application.Features.Incomes.v1.Commands;
 using PuppeteerSharp;
 
