22d91affcc0624d3c68dab81740c42cd501552ec
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-28 10:17:28 -0600

    feat: agregar tres tipos de ingresos

--- src/Application/Features/Incomes/v1/Handlers/RegisterIncome.Handler.cs
+++ src/Application/Features/Incomes/v1/Handlers/RegisterIncome.Handler.cs
@@ -46,17 +46,28 @@ namespace ERP.Core.Manager.Api.Application.Features.Incomes.v1.Handlers
             }
 
             //Obtener la nomina del la sucursal a la que pertence
-            var payrollOrdinary = await _unitOfWork.Payrolls.Entities 
+            var payroll = await _unitOfWork.Payrolls.Entities 
                 .Where(pay => pay.Status == PayrollStatus.Progress && pay.BranchId == collaboratorInformation.WorkingInformation.BranchInfo.Id)
                 .FirstOrDefaultAsync(cancellationToken);
 
-            if (payrollOrdinary is null)
+            if (payroll is null)
             {
                 logger.LogInformation("No se encontro una nomina en progreso");
                 return false;
             }
             else
             {
+
+                var ordinaryPayrollInformation = await _unitOfWork.OrdinaryPayrolls.Entities
+                    .Where(ord => ord.CollaboratorId == collaboratorInformation.Id && ord.PayrollId == payroll.Id)
+                    .FirstOrDefaultAsync(cancellationToken);
+
+
+                if (ordinaryPayrollInformation is null)
+                {
+                    return _errorManager.ThrowNotFound<bool>("No se encontro registro del colaborador en la nomina", "ERP:02");
+                }
+
                 //Verificarel si ese ingreso esta disponible
                 var Income = await _unitOfWork.TypesIncome.Entities
                     .Where(type => type.Id == request.TypeIncomeId && type.IsActive)
@@ -64,15 +75,62 @@ namespace ERP.Core.Manager.Api.Application.Features.Incomes.v1.Handlers
 
                 if (Income is null)
                 {
-                    // return _
+                    return _errorManager.ThrowBadRequest<bool>("Este tipo de ingreso no se encuentra disponible!", "ERP:03");
                 }
 
-                //Iniciando proceso de ingreso y contabilidad de nomina.
-                // switch(request.)
+                // Iniciando proceso de ingreso y contabilidad de nomina.
+                switch (Income.IncomeCode)
+                {
+                    case "ALW_MEAL" :
+                    {
+                        logger.LogInformation("Agregando ingreso de alimentación");
 
+                        ordinaryPayrollInformation.FoodTravelAllowance = request.IncomeAmount;
+                        ordinaryPayrollInformation.TotalToPay += request.IncomeAmount;
+                        
+                        await _unitOfWork.OrdinaryPayrolls.UpdateAsync(ordinaryPayrollInformation);
+                        await _unitOfWork.SaveChangesAsync(cancellationToken);
 
+                        logger.LogInformation("Proceso finalizado con exito!"); 
+                        
+                        return true;
+                    }
+                    case "ALW_HOUSING" :
+                    {
 
+                        logger.LogInformation("Agregando ingreso de hospedaje");
 
+                        ordinaryPayrollInformation.Lodging = request.IncomeAmount;
+                        ordinaryPayrollInformation.TotalToPay += request.IncomeAmount;
+                        
+                        await _unitOfWork.OrdinaryPayrolls.UpdateAsync(ordinaryPayrollInformation);
+                        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                        logger.LogInformation("Proceso finalizado con exito!");
+
+                        return true;
+                    }
+                    case "ALW_TRANSPORT":
+                    {
+
+                        logger.LogInformation("Agregando ingreso de transporte");
+
+                        ordinaryPayrollInformation.Lodging = request.IncomeAmount;
+                        ordinaryPayrollInformation.TotalToPay += request.IncomeAmount;
+                        
+                        await _unitOfWork.OrdinaryPayrolls.UpdateAsync(ordinaryPayrollInformation);
+                        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                        logger.LogInformation("Proceso finalizado con exito!");
+
+                        return true;
+                    }
+                    default:
+                    {
+                        _errorManager.ThrowBadRequest<bool>("Este tipo de ingreso no esta disponible", "ERP:04");
+                        break;   
+                    }
+                }
             }
 
             return true; 
