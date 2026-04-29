89c3a11a72467168d9b467215651ea55edc29a2f
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-29 15:28:27 -0600

    feat: agregar logo neutro de avasa logistic

--- src/ERP.Core.Manager.Api/obj/Debug/net10.0/ERP.Core.Manager.Api.AssemblyInfo.cs
+++ src/ERP.Core.Manager.Api/obj/Debug/net10.0/ERP.Core.Manager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d635b2a4dc0c2f0647b875d1e41dc071dd024e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021d9e78575c413221e431e5aed7cbeede9bb66e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Manager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
