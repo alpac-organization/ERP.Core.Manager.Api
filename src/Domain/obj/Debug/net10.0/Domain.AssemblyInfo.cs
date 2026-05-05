68c26acc741a7fe470f3d9170a03f48edaad3ba3
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-05 15:40:32 -0600

    feat: agregar cambios de conexion de base de datos

--- src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f961724de9e996171aa6bdaffaeee351af4356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09076a099492c96bfda671901dbda4aa7b231011")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
