9f86b31006ec04fbddf2134a5730ebbb0845b7fb
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-05 16:53:15 -0600

    feat: agregar cambios de migración de base de datos al core

--- src/Domain/Interfaces/Repositories/Payroll/IPayrollsRepository.cs
+++
@@ -1,9 +0,0 @@
-using ERP.Core.Manager.Api.Domain.Commons.Interfaces;
-
-namespace ERP.Core.Manager.Api.Domain.Interfaces.Repositories.Payroll
-{
-    public interface IPayrollsRepository : IRepository<Database.Domain.Entities.Payrolls.Payroll>
-    {
-        Task<Database.Domain.Entities.Payrolls.Payroll> InitializePayroll(Database.Domain.Entities.Payrolls.Payroll payload);
-    }
-}
