798b0f38d3c87adcb562dc19fe7b912cc012b776
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-06 17:08:12 -0600

    feat: guardar cambios

--- src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fcc9ac56882f671fbdbfac41a378f5c73a31de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e712b88da8446ab0bfe11b4c7b3b8882d11b595c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
