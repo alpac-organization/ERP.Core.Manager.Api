6dbb46a921a8a85b5524eb8cbc369edc1ef92f97
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-15 19:38:02 -0600

    feat: agregar dominio de acceso al api

--- src/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c418d4058cf26ad3921883d99e4c8b78a09be306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356e85615609d12d7d93d62fe8fa0754ccd9858f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ERP.Core.Manager.Api/Program.cs
+++ src/ERP.Core.Manager.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ViteLocalPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5174", "http://localhost:5173", "https://web-alpac.onrender.com")
+        policy.WithOrigins("http://localhost:5174", "http://localhost:5173", "https://web-alpac.onrender.com", "https://erp-vassalli-group-web.onrender.com/")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
