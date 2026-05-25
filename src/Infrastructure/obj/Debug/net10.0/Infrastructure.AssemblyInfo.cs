f9944a7fcf0aaf4132f1d6b4a08961d477f789e7
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-25 10:26:31 -0600

    feat: guardar cambios

--- src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c795d7d787414e3ec65cad5822551ca5ba682a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de18e0630bff8b265a3b0e13ae4151d1fde59d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
