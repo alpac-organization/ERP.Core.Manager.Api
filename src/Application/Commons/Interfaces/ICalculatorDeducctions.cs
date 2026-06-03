3c42a538976bac918d06a9663f07c720ecef1ab6
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-06-03 10:02:10 -0600

    feat: guardar cambios de reporteria

--- src/Application/Commons/Interfaces/ICalculatorDeducctions.cs
+++ src/Application/Commons/Interfaces/ICalculatorDeducctions.cs
@@ -5,10 +5,10 @@ namespace ERP.Core.Manager.Api.Application.Commons.Interfaces
 {
     public interface ICalculatorDeductions
     {
-        decimal CalculateAntique(decimal monthlySalary, DateOnly payrollStartDate, DateOnly collaboratorEntryDate);
         Task<decimal> CalculateInss(decimal GrossSalary, CancellationToken cancellationToken);
         Task<IrCalculationResult>CalculateIr(int NFortnight, decimal AccumulatedAccrued,decimal AccumulatedIR, decimal GrossSalary, CancellationToken cancellationToken, bool isSubsidy = false);
 
         Task RegisterOrdinaryPayrollForCollaborator(Guid payrollId, Collaborator collaborator, CancellationToken cancellationToken);
+        decimal CalculateAntique(decimal monthlySalary, DateOnly payrollStartDate, DateOnly collaboratorEntryDate);
     }
 }
