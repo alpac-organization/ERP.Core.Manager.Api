00c4237e42300e0b9be581e6ed35db92c00b002c
Gelzint vidaure <vidaureanto2002@gmail.com>
2026-06-24 10:25:53 -0600

    guardando avances en reporte de inss

--- src/Infrastructure/Services/ReportingServices.cs
+++ src/Infrastructure/Services/ReportingServices.cs
@@ -26,7 +26,7 @@ namespace ERP.Core.Manager.Api.Infrastructure.Services
         {
             int countCollaborators = await _unitOfWork.Collaborators.Entities
                 .Where(col => col.Status != CollaboratorStatus.Inactive)
-                .Where(col => col.DeletedAt.HasValue)
+                .Where(col => !col.DeletedAt.HasValue)
                 .CountAsync(c => c.CompanyId == collaborator.CompanyId);
 
             var validDeductions = await _unitOfWork.ValidityDeductions.Entities
@@ -34,7 +34,7 @@ namespace ERP.Core.Manager.Api.Infrastructure.Services
                 .ToListAsync(default);
 
             decimal inssLaborPercentage = validDeductions.FirstOrDefault(d => d.Type == TaxType.Inss)?.Value ?? 0.07m;
-            decimal inatecPercentage    = validDeductions.FirstOrDefault(d => d.Type == TaxType.Inatec)?.Value ?? 0.02m;
+            decimal inatecPercentage = validDeductions.FirstOrDefault(d => d.Type == TaxType.Inatec)?.Value ?? 0.02m;
 
             decimal inssPatronalPercentage = 0m;
 
@@ -47,24 +47,24 @@ namespace ERP.Core.Manager.Api.Infrastructure.Services
                 inssPatronalPercentage = validDeductions.FirstOrDefault(d => d.Type == TaxType.InssPatronal2)?.Value ?? 0.215m;
             }
 
-            decimal inssLaboralCalc     = salary * inssLaborPercentage;
-            decimal inatecCalc          = salary * inatecPercentage;
-            decimal inssPatronalCalc    = salary * inssPatronalPercentage;
-            decimal total               = inssLaboralCalc + inatecCalc + inssPatronalCalc;
+            decimal inssLaboralCalc = salary * inssLaborPercentage;
+            decimal inatecCalc = salary * inatecPercentage;
+            decimal inssPatronalCalc = salary * inssPatronalPercentage;
+            decimal total = inssLaboralCalc + inatecCalc + inssPatronalCalc;
 
 
             if (period == PayrollPeriod.FirstPeriod.ToString())
             {
                 var newInssRecord = new InssAccountingInformation()
                 {
-                    CollaboratorId  = collaborator.Id,
-                    PayrollId       = payrollId,
-                    InssLabor       = inssLaboralCalc,
-                    Inatec          = inatecCalc,
-                    InssPatronal    = inssPatronalCalc,
-                    Total           = total,
-                    Absence         = 0,
-                    DaysAbsence     = 0
+                    CollaboratorId = collaborator.Id,
+                    PayrollId = payrollId,
+                    InssLabor = inssLaboralCalc,
+                    Inatec = inatecCalc,
+                    InssPatronal = inssPatronalCalc,
+                    Total = total,
+                    Absence = 0,
+                    DaysAbsence = 0
                 };
 
                 await _unitOfWork.InssAccountingInformation.RegisterInssAccountingInformation(newInssRecord);
@@ -81,14 +81,14 @@ namespace ERP.Core.Manager.Api.Infrastructure.Services
                     var newInssRecord = new InssAccountingInformation()
                     {
 
-                        CollaboratorId  = collaborator.Id,
-                        PayrollId       = payrollId,
-                        InssLabor       = inssLaboralCalc,
-                        Inatec          = inatecCalc,
-                        InssPatronal    = inssPatronalCalc,
-                        Total           = total,
-                        Absence         = 0,
-                        DaysAbsence     = 0
+                        CollaboratorId = collaborator.Id,
+                        PayrollId = payrollId,
+                        InssLabor = inssLaboralCalc,
+                        Inatec = inatecCalc,
+                        InssPatronal = inssPatronalCalc,
+                        Total = total,
+                        Absence = 0,
+                        DaysAbsence = 0
                     };
                     await _unitOfWork.InssAccountingInformation.RegisterInssAccountingInformation(newInssRecord);
                 }
