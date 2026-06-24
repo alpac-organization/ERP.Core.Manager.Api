b474918d14f9215e50ae6f9f6b3a650fee37dfaa
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-06-24 10:54:46 -0600

    feat: agregar registros de sucursales nuevas

--- src/Application/Features/Branches/v1/Commands/RegisterBranch.Command.cs
+++ src/Application/Features/Branches/v1/Commands/RegisterBranch.Command.cs
@@ -6,7 +6,11 @@ namespace ERP.Core.Manager.Api.Application.Features.Branches.v1.Commands
     public class RegisterBranchCommand: BaseRequest, IRequest<bool>
     {
         public string? BrachName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
         public string? BranchCode { get; set; }
         public string? Descripcion { get; set; }
+
+        public bool HasWarehouse { get; set; }
     }
 }
