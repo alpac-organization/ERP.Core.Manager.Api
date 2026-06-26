cf52eadbd864fa9d08256ab94514105946903c49
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-06-26 09:20:43 -0600

    feat: agregar caso de baja colaborador

--- src/Application/Features/Payroll/v1/Handlers/ClosePayrollProcess.Handler.cs
+++ src/Application/Features/Payroll/v1/Handlers/ClosePayrollProcess.Handler.cs
@@ -44,6 +44,7 @@ namespace ERP.Core.Manager.Api.Application.Features.Payroll.v1.Handlers
                     pay.Id == request.PayrollId && pay.Status == PayrollStatus.Progress
                 )
                 .Include(pay => pay.OrdinaryPayrolls)
+                    .ThenInclude(or => or.Collaborator)
                 .FirstOrDefaultAsync(cancellationToken);
 
             if (payroll is null)
@@ -73,6 +74,16 @@ namespace ERP.Core.Manager.Api.Application.Features.Payroll.v1.Handlers
             //Realizar todo aquel, registro de deducciones y pagos realizados del colaborador
             foreach (var collaborator in registers)
             {
+                //Verificar si el colaborador se encuentra despedido.
+
+                if (collaborator.Collaborator.HasBeenFired)
+                {
+                    //Este colaborador has sido despedido ya no continua en el progreso de nomina
+                    _logger.LogInformation("El colaborador con identification: {identification}, has sido dado de baja", collaborator.Collaborator.IdentificationNumber);
+                    continue;
+                }
+
+
                 var deductionsActive = await _unitOfWork.Deductions.Entities
                     .Where(deduction => deduction.CollaboratorId == collaborator.Id)
                     .Where(deduction => deduction.Status == DeductionStatus.Progress)
