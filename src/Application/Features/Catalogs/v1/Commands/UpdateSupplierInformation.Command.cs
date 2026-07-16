9c6d826854fc4e582c9f064e24eaddc8e5974d5f
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-07-16 17:41:23 -0600

    feat: agregar path de actualización de proveedores

---
+++ src/Application/Features/Catalogs/v1/Commands/UpdateSupplierInformation.Command.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using ERP.Core.Database.Domain.Enums;
+using ERP.Core.Manager.Api.Domain.Entities.Bases;
+
+namespace ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Commands
+{
+    public class UpdateSupplierInformationCommand : BaseRequest, IRequest<bool>
+    {
+        public Guid SupplierId { get; set; }
+        public string? Address { get; set; }
+        public string? EmailSupport { get; set; }
+        public string? ContactName { get; set; }
+        public string? ContactEmail { get; set; }
+        public string? ContactPhoneNumber { get; set; }
+        
+        public string? SuppliersLegalName { get; set; }
+        public string? IdentificationNumber { get; set; }
+
+        public ConstitutionType? ConstitutionType { get; set; }
+        public IdentificationType? IdentificationType { get; set; }
+    }
+}
