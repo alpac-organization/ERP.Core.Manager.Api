2029851dd223d1a51368840a20dceae2367a8d81
David Cortes <davidcortes0209@gmail.com>
2026-07-24 14:29:14 -0600

    feat(#343): closed: #343 - endpoint para ingresar productos

--- src/Application/Commons/Mappings/ProductsProfile.cs
+++ src/Application/Commons/Mappings/ProductsProfile.cs
@@ -1,6 +1,7 @@
 
 using AutoMapper;
 using ERP.Core.Database.Domain.Entities.Warehouse;
+using ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Commands;
 using ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Dtos;
 
 namespace ERP.Core.Manager.Api.Application.Commons.Mappings;
@@ -11,5 +12,12 @@ public class ProductsProfile : Profile
     {
         CreateMap<Product, ProductDto>()
             .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id));
+
+        CreateMap<RegisterProductCommand, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.DeletedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Category, opt => opt.Ignore())
+            .ForMember(dest => dest.QuoteDetails, opt => opt.Ignore());
     }
 }
---
+++ src/Application/Features/Catalogs/v1/Validators/RegisterProduct.Validator.cs
@@ -0,0 +1,32 @@
+using ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Commands;
+using FluentValidation;
+
+namespace ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Validators;
+
+public class RegisterProductValidator : AbstractValidator<RegisterProductCommand>
+{
+    public RegisterProductValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("El id de usuario es requerido.")
+            .NotEqual(Guid.Empty).WithMessage("El id de usuario no es válido");
+
+        RuleFor(x => x.CompanyId)
+            .NotEmpty().WithMessage("El id de la empresa es requerido.")
+            .NotEqual(Guid.Empty).WithMessage("El id de la empresa no es válido.");
+
+        RuleFor(x => x.ModuleCode)
+                .NotEmpty().WithMessage("El código de módulo es requerido.");
+
+        RuleFor(x => x.ProductName)
+            .NotEmpty().WithMessage("El nombre del producto es obligatorio.")
+            .MaximumLength(30).WithMessage("El nombre del producto no puede exceder los 200 caracteres.");
+
+        RuleFor(x => x.UsageType)
+            .IsInEnum().WithMessage("El tipo de uso es inválido.");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("La categoría es obligatoria.")
+            .NotEqual(Guid.Empty).WithMessage("El id de la categoría no es válido.");
+    }
+}
