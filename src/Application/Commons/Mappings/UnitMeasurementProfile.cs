18960f56ab760ead2469082c1f3777ea0b5786d7
Angel-Delg <lariosangel062@gmail.com>
2026-08-25 16:17:04 -0600

    fix: agregar corrección para registro de dispositivo

--- src/Application/Commons/Mappings/UnitMeasurementProfile.cs
+++ src/Application/Commons/Mappings/UnitMeasurementProfile.cs
@@ -1,7 +1,6 @@
 using AutoMapper;
 using ERP.Core.Database.Domain.Entities.Catalogs;
 using ERP.Core.Manager.Api.Application.Features.Catalogs.v1.Dtos;
-using ERP.Core.Manager.Api.Application.Features.Modules.v1.Dtos;
 
 namespace ERP.Core.Manager.Api.Application.Commons.Mappings
 {
